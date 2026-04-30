fb9f0a7d975a9b68d5a865b6e92e92d001aecca7
prayag0925 <prayag0925@gmail.com>
2026-04-30 12:47:22 +0530

    Update server.js

--- final-server/server.js
+++ final-server/server.js
@@ -1,22 +1,40 @@
+
 const http = require("http");
 const url = require("url");
+
 
 const homeRoute = require("./routes/home");
 const aboutRoute = require("./routes/about");
 const contactRoute = require("./routes/contact");
 const notFoundRoute = require("./routes/notFound");
+const userRoute = require("./routes/user");
+
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  const pathname = url.parse(req.url, true).pathname;
 
-  if (pathname === "/") homeRoute(res);
-  else if (pathname === "/about") aboutRoute(res);
-  else if (pathname === "/contact") contactRoute(res);
-  else notFoundRoute(res);
+const server = http.createServer((req, res) => {
+
+
+  const parsedUrl = url.parse(req.url, true);
+  const pathname = parsedUrl.pathname;
+  const query = parsedUrl.query;
+
+  if (pathname === "/") {
+    homeRoute(res);
+  } else if (pathname === "/about") {
+    aboutRoute(res);
+  } else if (pathname === "/contact") {
+    contactRoute(res);
+  } else if (pathname === "/user") {
+    userRoute(res, query);
+  } else {
+    notFoundRoute(res);
+  }
+
 });
 
-server.listen(3000, () => {
-  console.log(` Server running at http://localhost:3000`);
+
+server.listen(PORT, () => {
+  console.log(` Server running at http://localhost:${PORT}`);
 });
